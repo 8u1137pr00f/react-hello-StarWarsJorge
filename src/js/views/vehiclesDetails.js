9870bb38886f7ecb07b4a51e00c6639d3005acc3
8u1137pr00f <162195925+8u1137pr00f@users.noreply.github.com>
2024-06-22 19:07:45 +0200

    Create vehiclesDetails.js

---
+++ src/js/views/vehiclesDetails.js
@@ -0,0 +1,15 @@
+import React, { useContext }from "react";
+
+import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+
+export const VehicleDetails = () => {
+
+
+	return(
+
+	<div className="text-center mt-5">
+		<h1>vehicles!</h1>
+	</div>
+)};
